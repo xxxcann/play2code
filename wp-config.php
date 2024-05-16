fcde4037ccb1c2f3b764abe1524d2eac19c1adcd
xxcannn <play2code2024@gmail.com>
2024-05-16 16:27:41 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,26 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+// define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+// define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+// define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+// define( 'DB_HOST', 'localhost' );
+
+// sca
+$mysql_url = parse_url($_ENV["DATABASE_URL"]);
+$db = substr($mysql_url['path'], 1);
+
+define('DB_NAME', $db);
+define('DB_USER', $mysql_url['user']);
+define('DB_PASSWORD', $mysql_url['pass']);
+define('DB_HOST', $mysql_url['host'] . ":" . $mysql_url['port']);
+
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
